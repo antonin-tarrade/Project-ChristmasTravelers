32e3bb16eb42df4b13a545333859a92b6b3d1fb8
antonin-tarrade <antotarrade@gmail.com>
2023-12-03 17:12:46 +0100

    Added UI Scripting elements + Generate Player pool based on number of players

--- ChristmasTravelers/Assets/Scripts/Ui/ChooseCharacterManager.cs
+++ ChristmasTravelers/Assets/Scripts/Ui/ChooseCharacterManager.cs
@@ -1,8 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Resources;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XInput;
 using UnityEngine.UI;
 
 public class ChooseCharacterManager : MonoBehaviour
@@ -11,16 +15,20 @@ public class ChooseCharacterManager : MonoBehaviour
     public static ChooseCharacterManager instance;
 
     private GameObject[] allCharacters;
+    private GameManager gameManager;
 
 
     [SerializeField] Transform canvas;
     [SerializeField] GameObject characterUiMini;
     [SerializeField] GameObject characterUiBig;
+    [SerializeField] GameObject playerPool;
 
     private Transform allCharactersPool;
-    private Transform player1Pool;
-    private Transform player2Pool;
+    private Transform allPlayersPool;
 
+    private List<GameObject> playersPool;
+
+    private int nbOfPlayers;
 
 
     private void Awake()
@@ -36,13 +44,24 @@ public class ChooseCharacterManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+        nbOfPlayers = InputSystem.devices.OfType<XInputController>().Count();
+
+        gameManager = GameManager.instance;
+
+        for (int i = 1; i < nbOfPlayers; i++){
+            Player newPlayer = new Player
+            {
+                name = "Player" + i
+            };
+            gameManager.players.Add(newPlayer);
+        }
+
         allCharacters = Resources.LoadAll<GameObject>("Characters");
         allCharactersPool = canvas.Find("AllCharactersPool");
-        player1Pool = canvas.Find("Player1Pool");
-        player2Pool = canvas.Find("Player2Pool");
-
+        allPlayersPool = canvas.Find("AllPlayersPool");
+        playersPool = new List<GameObject>();
         InitCharacterPool();
-    
     }
 
     // Update is called once per frame
@@ -56,17 +75,29 @@ public class ChooseCharacterManager : MonoBehaviour
         foreach (GameObject ch in allCharacters) {
             GameObject characterUi = Instantiate(characterUiMini,allCharactersPool.transform);
             characterUi.GetComponentInChildren<TextMeshProUGUI>().text = ch.name;
-            characterUi.transform.parent = allCharactersPool;
-            characterUi.GetComponent<Button>().onClick.AddListener(() => SelectCharacter(ch));
+            characterUi.transform.SetParent(allCharactersPool);
+            characterUi.GetComponent<Button>().onClick.AddListener(() => SelectCharacter(ch,1));
         }
 
     }
 
-    private void SelectCharacter(GameObject character){
-        GameObject characterUi = Instantiate(characterUiBig,player1Pool.transform);
-        characterUi.GetComponentInChildren<TextMeshProUGUI>().text = character.name;
-        characterUi.transform.parent = player1Pool;
+    private void SelectCharacter(GameObject character,int playerNb){
 
+        GameObject pool;
+        
+        if (playersPool.Count < playerNb){
+            pool = Instantiate(playerPool,allPlayersPool.transform);
+            pool.transform.SetParent(allPlayersPool);
+            playersPool.Add(pool);
+        } else {
+            pool = playersPool[playerNb-1];
+        }
+        GameObject characterUi = Instantiate(characterUiBig,pool.transform);
+        characterUi.GetComponentInChildren<TextMeshProUGUI>().text = character.name;
+        characterUi.transform.SetParent(pool.transform);
     }
+
+
+
 
 }
