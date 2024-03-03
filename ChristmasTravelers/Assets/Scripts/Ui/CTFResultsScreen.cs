385900ac95d3f8fbbf1bab0399a68c2aae1c7d8e
Antonin Tarrade <antotarrade@gmail.com>
2024-03-03 19:41:55 +0100

    merge

--- ChristmasTravelers/Assets/Scripts/Ui/CTFResultsScreen.cs
+++ ChristmasTravelers/Assets/Scripts/Ui/CTFResultsScreen.cs
@@ -13,9 +13,6 @@ public class CTFResultsScreen : ResultsScreen
     public override void Display(Player[] rankedPlayers)
     {
         text.text = rankedPlayers[0].name + " won !";
-        GameObject characterInstance = Instantiate(ResultCharacterPrefab);
-        characterInstance.GetComponent<Image>().sprite = rankedPlayers[0].team.chickenSpriteLibrary.GetSprite("UI", "big");
-        characterInstance.transform.SetParent(CharacterContainer, false);
         }
 
 
